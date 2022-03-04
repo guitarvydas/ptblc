935c2c61c6ea07eb00c7108e3bf9cfe3050b551a
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-04 08:53:48 -0500

    truefalsenil

--- run.bash
+++ run.bash
@@ -1,3 +1,7 @@
 prep=~/projects/prep/prep
-$prep '%pair' '.' pair.ohm pair.glue
+$prep '\$pair' '.' pair.ohm pair.glue |\
+    $prep '\$true' '.' truefalsenil.ohm truefalsenil.glue |\
+    $prep '\$false' '.' truefalsenil.ohm truefalsenil.glue |\
+    $prep '\$nil' '.' truefalsenil.ohm truefalsenil.glue
+    
 
