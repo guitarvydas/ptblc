68f55f6bc9fd65d73aedcd57d71dad59d40e738f
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-05 07:26:33 -0500

    identity transform

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 all:
 	./run.bash <test.bl
+	./run.bash <test0.bl
 
--- run.bash
+++ run.bash
@@ -1,5 +1,6 @@
 prep=~/projects/prep/prep
 $prep '\$pair' '.' pair.ohm pair.glue |\
-    $prep '.' '$' truefalsenil.ohm truefalsenil.glue --stop=1
+    $prep '.' '$' truefalsenil.ohm truefalsenil.glue --stop=1 |\
+    $prep '.' '$' blc.ohm blc.glue --stop=1
     
 
