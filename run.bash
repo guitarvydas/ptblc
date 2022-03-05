86d899906b4c2240680cee16a67bdd99d5074af9
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-05 07:49:05 -0500

    display intermediate preprocessed result

--- run.bash
+++ run.bash
@@ -2,8 +2,10 @@ prep=~/projects/prep/prep
 cdir=`pwd`
 cat - >/tmp/_temp
 cat /tmp/_temp
+echo
 $prep '\$pair' '.' pair.ohm pair.glue </tmp/_temp |\
-    $prep '.' '$' truefalsenil.ohm truefalsenil.glue --stop=1 --support=$cdir/support.js |\
-    $prep '.' '$' blc.ohm blc.glue --stop=1 --support=$cdir/support.js
+    $prep '.' '$' truefalsenil.ohm truefalsenil.glue --stop=1 --support=$cdir/support.js >/tmp/_preprocessed
+cat /tmp/_preprocessed
+$prep '.' '$' blc.ohm blc.glue --stop=1 --support=$cdir/support.js </tmp/_preprocessed
     
 
