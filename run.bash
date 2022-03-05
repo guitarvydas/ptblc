8502f758898b8f734a3254636ceaeab1d06af639
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-05 07:57:18 -0500

    annotate output

--- run.bash
+++ run.bash
@@ -1,11 +1,17 @@
 prep=~/projects/prep/prep
 cdir=`pwd`
+
+echo original input
 cat - >/tmp/_temp
 cat /tmp/_temp
 echo
+
+echo macro expansion
 $prep '\$pair' '.' pair.ohm pair.glue </tmp/_temp |\
     $prep '.' '$' truefalsenil.ohm truefalsenil.glue --stop=1 --support=$cdir/support.js >/tmp/_preprocessed
 cat /tmp/_preprocessed
+
+echo convert to bits in ASCII
 $prep '.' '$' blc.ohm blc.glue --stop=1 --support=$cdir/support.js </tmp/_preprocessed
     
 
