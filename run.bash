5fe17f0145e053317b998fc4215c0b600de963b9
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-05 08:10:15 -0500

    local copy of prep tool

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
-prep=~/projects/prep/prep
+prep=./prep/prep
 cdir=`pwd`
 
 echo original input
@@ -7,11 +7,11 @@ cat /tmp/_temp
 echo
 
 echo macro expansion
-$prep '\$pair' '.' pair.ohm pair.glue </tmp/_temp |\
+$prep '.' '$' pair.ohm pair.glue --stop=1 </tmp/_temp |\
     $prep '.' '$' truefalsenil.ohm truefalsenil.glue --stop=1 --support=$cdir/support.js >/tmp/_preprocessed
 cat /tmp/_preprocessed
 
-echo convert to bits in ASCII
+echo convert to bits as ASCII
 $prep '.' '$' blc.ohm blc.glue --stop=1 --support=$cdir/support.js </tmp/_preprocessed
     
 
