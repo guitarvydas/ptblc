ade8985a9d57e8bd9bd96de076c8322f4d74bb20
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-08 16:25:32 -0500

    ${lb} ${rb}

--- run.bash
+++ run.bash
@@ -14,8 +14,11 @@ cat /tmp/_preprocessed
 echo convert to bits as ASCII
 $prep '.' '$' blc.ohm blc.glue --stop=1 --support=$cdir/support.js </tmp/_preprocessed
 
-echo convert to Scheme
-#$prep '.' '$' blc.ohm blc2scm.glue --stop=1 --support=$cdir/support.js --trace </tmp/_preprocessed
-$prep '.' '$' blc.ohm blc2scm.glue --stop=1 --support=$cdir/support.js </tmp/_preprocessed
+echo convert to Apply
+$prep '.' '$' blc.ohm blc2apply.glue --stop=1 --support=$cdir/support.js </tmp/_preprocessed
+
+# echo convert to Scheme
+# #$prep '.' '$' blc.ohm blc2scm.glue --stop=1 --support=$cdir/support.js --trace </tmp/_preprocessed
+# $prep '.' '$' blc.ohm blc2scm.glue --stop=1 --support=$cdir/support.js </tmp/_preprocessed
 
 
